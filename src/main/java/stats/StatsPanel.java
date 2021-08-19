d17c813fac985739e00ce317172459a44decbcf7
Dario Tecchia <dariotecchia@gmail.com>
2021-08-19 12:40:37 +0200

    add total class number in StatsPanel

--- src/main/java/stats/StatsPanel.java
+++ src/main/java/stats/StatsPanel.java
@@ -27,6 +27,7 @@ public class StatsPanel extends JPanel {
     this.statsInfo.add(new JLabel("Session ID: " + this.session.getID()));
     this.statsInfo.add(new JLabel(this.session.getKind() + " Detection"));
     this.statsInfo.add(new JLabel("Execution time: " + this.session.getExecutionTime() + "ms"));
+    this.statsInfo.add(new JLabel("Number of classes of the project: " + this.session.getnOfTotalClasses()));
     this.statsInfo.add(new JLabel("Number of General Fixture founded: " + this.session.getNOfGF()));
     this.statsInfo.add(new JLabel("Number of Eager Test founded: " + this.session.getNOfET()));
     this.statsInfo.add(new JLabel("Number of Lack Of Cohesion founded: " + this.session.getNOfLOC()));
