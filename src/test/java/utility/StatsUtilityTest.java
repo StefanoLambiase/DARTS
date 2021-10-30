1630481f9a2d6a6071f83ce4be935bc8070d23d4
Dario Di Dario <dariodidario97@gmail.com>
2021-10-30 03:04:57 +0200

    update test md5

--- src/test/java/utility/StatsUtilityTest.java
+++ src/test/java/utility/StatsUtilityTest.java
@@ -13,7 +13,8 @@ public class StatsUtilityTest {
     public void md5_FilledParameter_Test(){
         String dayGetTime = "1635440679790";
         String oracle = "2E4CB449D08B2757D0E8C7FC100C8987";
-        assertEquals(oracle, StatsUtility.md5(dayGetTime + System.getProperty("user.name")));
+        String userGetName = "UniSa";
+        assertEquals(oracle, StatsUtility.md5(dayGetTime + userGetName));
     }
 
     @Test
